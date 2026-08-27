3971f2037fa962ae3d0ad75b9c1a42fd480acf34
Nicolas Dos Santos Martini <snickmartini94@gmail.com>
2026-08-26 21:44:17 -0300

    forTabuada

---
+++ Exercicio05.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("Digite um número:");
+int numero = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
+}
+
+Console.ReadKey();
