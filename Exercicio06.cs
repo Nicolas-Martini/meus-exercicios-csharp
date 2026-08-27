bd87f132ecfdd91855df6bb8632b59bc2170a76a
Nicolas Dos Santos Martini <snickmartini94@gmail.com>
2026-08-26 21:47:55 -0300

    Exercicio06 - For/Tabuada

--- Exercicio06.cs
+++ Exercicio06.cs
@@ -3,7 +3,7 @@ int numero = int.Parse(Console.ReadLine());
 
 for (int i = 0; i <= 10; i++)
 {
-    Console.WriteLine($"{numero} x {i} = {numero * i}");
+    Console.WriteLine($"{numero} x {i} = {numero * i}"); 
 }
  
 Console.ReadKey(); 
